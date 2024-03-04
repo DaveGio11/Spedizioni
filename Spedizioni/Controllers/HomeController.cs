62619ad320a090253d907fb1ec9d532f0419a929
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2024-03-04 15:43:34 +0100

    Aggiungere i file di progetto.

---
+++ Spedizioni/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace Spedizioni.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CheckCodFiscale(string CodFisc)
+        {
+            string pattern = @"^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(CodFisc))
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult CheckPartitaIva(string PartitaIva)
+        {
+            string pattern = @"^\d{11}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(PartitaIva))
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        DateTime miaData = DateTime.Today;
+        public ActionResult CheckDataSpedizione(DateTime data)
+        {
+            miaData = data;
+            if (data >= DateTime.Today)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult CheckDataConsegna(DateTime data)
+        {
+            if (data > miaData)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+
+
+}
+}
---
+++ Spedizioni/Models/Spedizione.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Spedizioni.Models
+{
+    public class Spedizione
+    {
+        public int IdSpedizione { get; set; }
+        public int IdCliente { get; set; }
+        [DisplayName("Crea codice tracciamento")]
+        [Required(ErrorMessage = "Il codice è obbligatorio")]
+        [StringLength(12, MinimumLength = 12,
+        ErrorMessage = "Il codice deve essere di 12 caratteri")]
+        public string CodTraccia { get; set; }
+
+        public DateTime DataSpedizione { get; set; }
+        [DisplayName("Inserisci il peso della spedizione")]
+        [Required(ErrorMessage = "Il codice è obbligatorio")]
+        public decimal PesoSpedizione { get; set; }
+        [DisplayName("Inserisci la citta di destinazione")]
+        [Required(ErrorMessage = "la destinazione e' obbligatoria")]
+        [StringLength(50, MinimumLength = 3,
+        ErrorMessage = "la citta deve essere compresa tra 3 e 50 caratteri")]
+        public string CittaDestinazione { get; set; }
+        [DisplayName("Inserisci l'indirizzo di destinazione")]
+        [Required(ErrorMessage = "l'indirizzo di destinazione e' obbligatorio")]
+        public string IndirizzoDestinazione { get; set; }
+        [DisplayName("Inserisci il nominativo del destinatario")]
+        [Required(ErrorMessage = "Il nominativo del destinatario e' obbligatorio")]
+        [StringLength(255, MinimumLength = 3,
+            ErrorMessage = "Il nominativo del destinatario deve essere compreso tra 3 e 255 caratteri")]
+        public string NomeDestinatario { get; set; }
+        [DisplayName("Inserisci il costo della spedizione")]
+        [Required(ErrorMessage = "Il costo della spedizione e' obbligatorio")]
+        public decimal CostoSpedizione { get; set; }
+        public DateTime DataConsegna { get; set; }
+    }
+}
